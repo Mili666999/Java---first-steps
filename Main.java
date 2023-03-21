0fa85e0efd92a9755704ff810eeb8523f0738f7a
Mili666999 <128092036+Mili666999@users.noreply.github.com>
2023-03-21 16:45:12 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Unesi iznos racuna: ");
-        double iznos = scan.nextDouble();
-        System.out.println("Baksa je " + iznos * 15 / 100 + " novaca.");
+        System.out.print("Unesi temperaturu vode: ");
+        int temp = scan.nextInt();
+        if (temp >= 100)
+            System.out.print("Ejte vi, voda vri!");
+        else System.out.print("Jos nista...");
 
     }
 }
